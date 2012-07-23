81dee67a9b9c18c56a8cd6c3023c886a4ae58336
fatfish <fatfish2010@sina.cn>
2012-07-23 12:02:42 +0800

    月即将退出营运车辆确认流程后台代码实现

--- bc-business-workflow-confirmretiredcars/src/main/java/cn/bc/business/workflow/confirmretiredcars/ServiceTask4BatchStartCarExecutionProcess.java
+++ bc-business-workflow-confirmretiredcars/src/main/java/cn/bc/business/workflow/confirmretiredcars/ServiceTask4BatchStartCarExecutionProcess.java
@@ -1,6 +1,7 @@
 package cn.bc.business.workflow.confirmretiredcars;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.activiti.engine.RuntimeService;
@@ -9,7 +10,8 @@ import org.activiti.engine.delegate.JavaDelegate;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.StringUtils;
+
+import cn.bc.core.util.JsonUtils;
 
 /**
  * 批量发起交车流程的服务任务
@@ -17,10 +19,10 @@ import org.springframework.util.StringUtils;
  * @author dragon
  * 
  */
-public class ServiceTask4BatchStartCarExitOperationProcess implements
+public class ServiceTask4BatchStartCarExecutionProcess implements
 		JavaDelegate {
 	private static final Log logger = LogFactory
-			.getLog(ServiceTask4BatchStartCarExitOperationProcess.class);
+			.getLog(ServiceTask4BatchStartCarExecutionProcess.class);
 	private RuntimeService runtimeService;
 
 	@Autowired
@@ -29,27 +31,31 @@ public class ServiceTask4BatchStartCarExitOperationProcess implements
 	}
 
 	public void execute(DelegateExecution execution) throws Exception {
-		// 获取交车车辆列表 TODO 获取更多信息
-		Object[] cars = (Object[]) execution.getVariable("exitOperationCars");
+		String carsStr = (String) execution.getVariable("list_lv_cars");
 		if (logger.isDebugEnabled()) {
 			logger.debug("from=" + execution.getProcessInstanceId());
 			logger.debug("exitOperationCars="
-					+ StringUtils.arrayToCommaDelimitedString(cars));
+					+ carsStr);
 		}
 
-		// 循环每一台车发起交车流程
+		List<Object> carList=(List<Object>) JsonUtils.toCollection(carsStr);
+		
+		
 		Map<String, Object> variables;
-		for (Object car : cars) {
+		for(Object car:carList){
 			// 设置流程变量
 			variables = new HashMap<String, Object>();
 			variables.put("from", execution.getProcessInstanceId());// 来源信息
 			variables.put("car", car);// 车辆信息
-
-			// TODO 设置更多变量信息
-
-			// 启动流程
-			// runtimeService.startProcessInstanceByKey("CarExitOperationProcess",
-			// from + ":" + car, variables);
+			@SuppressWarnings("unchecked")
+			Map<String,String> carMap=(Map<String,String>) car;
+			if("fireCarRetiredProcesscarMap".equals(carMap.get("executionType"))){
+				//发起车辆退出流程
+				runtimeService.startProcessInstanceByKey("CarExitOperationProcess", "CarExitOperationProcess:" + car, variables);
+			}else if("fireCarRenewProcess".equals(carMap.get("executionType"))){
+				//发起车辆续保流程
+				runtimeService.startProcessInstanceByKey("CarRenewProcess", "CarRenewProcess:" + car, variables);
+			}
 		}
 	}
 }
