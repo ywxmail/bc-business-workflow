347166c34f0e03b5c267a6bb974e24d73b1f533f
fatfish <fatfish2010@sina.cn>
2013-01-09 15:00:15 +0800

    司机入职审批流程自动发起流程监听器修改

--- bc-business-workflow-carmanentry/src/main/java/cn/bc/business/workflow/carmanentry/delegate/StartCarManDealWithCertListener.java
+++ bc-business-workflow-carmanentry/src/main/java/cn/bc/business/workflow/carmanentry/delegate/StartCarManDealWithCertListener.java
@@ -6,15 +6,17 @@ package cn.bc.business.workflow.carmanentry.delegate;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.activiti.engine.RuntimeService;
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.ExecutionListener;
 import org.activiti.engine.impl.el.Expression;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import cn.bc.core.util.JsonUtils;
+import cn.bc.business.tempdriver.domain.TempDriver;
 import cn.bc.core.util.SpringUtils;
+import cn.bc.workflow.domain.WorkflowModuleRelation;
+import cn.bc.workflow.service.WorkflowModuleRelationService;
+import cn.bc.workflow.service.WorkflowService;
 
 /**
  * 自动发起司机办证流程监听器
@@ -26,10 +28,12 @@ public class StartCarManDealWithCertListener implements ExecutionListener {
 	private static final Log logger = LogFactory
 			.getLog(StartCarManDealWithCertListener.class);
 
-	private RuntimeService runtimeService;
+	private WorkflowModuleRelationService  workflowModuleRelationService;
+	private WorkflowService workflowService;
 
 	public StartCarManDealWithCertListener(){
-		runtimeService=SpringUtils.getBean(RuntimeService.class);
+		workflowService=SpringUtils.getBean(WorkflowService.class);
+		workflowModuleRelationService=SpringUtils.getBean(WorkflowModuleRelationService.class);
 	}
 	
 	/**
@@ -37,9 +41,7 @@ public class StartCarManDealWithCertListener implements ExecutionListener {
 	 */
 	private Expression key;
 	
-	/**
-	 * 司机办证流程需要的变量 配置信息为“{"自定义key1":"全局变量名1","自定义key2":"全局变量名2","自定义key3":"全局变量名3"...}" 
-	 */
+	@SuppressWarnings("unused")
 	private Expression vKeys;
 
 	public void notify(DelegateExecution execution) throws Exception {
@@ -47,8 +49,6 @@ public class StartCarManDealWithCertListener implements ExecutionListener {
 			logger.debug("processInstanceId=" + execution.getProcessInstanceId());
 			logger.debug("key="
 					+ key.getExpressionText());
-			logger.debug("vKeys="
-					+ vKeys.getExpressionText());
 		}
 		
 		//没此公用变量 不自动发起流程；
@@ -61,19 +61,29 @@ public class StartCarManDealWithCertListener implements ExecutionListener {
 		if(executionType.equals("manual")||executionType.equals("noProcess"))
 			return;
 		
-		Map<String, Object> _vKeys=JsonUtils.toMap(vKeys.getExpressionText());
-		
 		Map<String, Object> variables=new HashMap<String, Object>();
 		
+		//设置名称
+		variables.put("name", execution.getVariable("name").toString());
+		//设置身份证号
+		variables.put("certIdentity",execution.getVariable("certIdentity").toString());
+		//设置司机招聘ID
+		variables.put("tempDriver_id",execution.getVariable("tempDriver_id").toString());
 		//设置司机入职流程的Id
 		variables.put("CarManEntry_ProcessInstanceId", execution.getProcessInstanceId());
+
+		//发起办证流程
+		String procInstId=workflowService.startFlowByKey(key.getExpressionText(), variables);
 		
-		for(String vKey: _vKeys.keySet()){
-			if(execution.hasVariable(_vKeys.get(vKey).toString())){
-				variables.put(vKey,execution.getVariable(_vKeys.get(vKey).toString()));
-			}
-		}
-		runtimeService.startProcessInstanceByKey(key.getExpressionText(), variables);
+		//审批流程加入办证流程的实例ID
+		execution.setVariable("RequestDerviceCertficate_ProcessInstanceId", procInstId);
+		
+		//创建办证流程与司机招聘的流程关系
+		WorkflowModuleRelation workflowModuleRelation=new WorkflowModuleRelation();
+		workflowModuleRelation.setMid(Long.valueOf(execution.getVariable("tempDriver_id").toString()));
+		workflowModuleRelation.setPid(procInstId);
+		workflowModuleRelation.setMtype(TempDriver.WORKFLOW_MTYPE);
+		this.workflowModuleRelationService.save(workflowModuleRelation);
 		
 	}
 
